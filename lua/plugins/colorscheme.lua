7325845288a89924b3017af27f5722adabb2efb3
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2023-12-21 12:55:10 +0100

    wip following devaslife video

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
+}
+
+-- return {
+--   "oxfist/night-owl.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     -- load the colorscheme here
+--     vim.cmd.colorscheme("night-owl")
+--   end,
+-- }
