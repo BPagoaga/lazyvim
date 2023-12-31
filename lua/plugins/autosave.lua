4116976c31ba725e4458160a4b2b05d78d8dbe45
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2023-12-31 15:26:20 +0100

    change autosave config

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -3,7 +3,7 @@ return {
   cmd = "ASToggle", -- optional for lazy loading on command
   event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
   opts = {
-    -- your config goes here
-    -- or just leave it empty :)
+    trigger_events = { "InsertLeave", "FocusLost", "WinLeave", "TabLeave", "BufLeave" },
+    debounce_delay = 300,
   },
 }
