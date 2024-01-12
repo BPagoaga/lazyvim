f3766816e2dbb7bf942b044898c807dce7bd3cab
BPagoaga <bernard.pagoaga@gmail.com>
2024-01-12 22:24:33 +0100

    change color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,22 @@
-return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    lualine_bold = true,
-    transparent = true,
-    -- on_colors = function(colors)
-    -- colors.bg = "#2a2a40"
-    -- colors.fg = "#dfdfe2"
-    -- colors.bg_popup = "#0a0a23"
-    -- colors.bg_sidebar = "#0a0a23"
-    -- colors.bg_float = "#0a0a23"
-    -- colors.bg_statusline = "#0a0a23"
-    --
-    -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#0a0a23" })
-    -- end,
-  },
-}
+-- return {
+--   "craftzdog/solarized-osaka.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {
+--     lualine_bold = true,
+--     transparent = true,
+--     -- on_colors = function(colors)
+--     -- colors.bg = "#2a2a40"
+--     -- colors.fg = "#dfdfe2"
+--     -- colors.bg_popup = "#0a0a23"
+--     -- colors.bg_sidebar = "#0a0a23"
+--     -- colors.bg_float = "#0a0a23"
+--     -- colors.bg_statusline = "#0a0a23"
+--     --
+--     -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#0a0a23" })
+--     -- end,
+--   },
+-- }
 --
 -- return {
 --   "oxfist/night-owl.nvim",
@@ -27,15 +27,20 @@ return {
 --     vim.cmd.colorscheme("night-owl")
 --   end,
 -- }
--- return {
---   "catppuccin/nvim",
---   name = "catppuccin",
---   lazy = true,
---   priority = 1000,
---   opts = {
---     transparent_background = true,
---   },
---   config = function()
---     vim.cmd.colorscheme("catppuccin-mocha")
---   end,
--- }
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
