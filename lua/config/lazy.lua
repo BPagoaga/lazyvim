5fc1832d5c6f8b9927f0f4a12da9cd17094564e8
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2024-02-02 09:04:32 +0100

    replace symbol outline by aerial

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,9 @@ require("lazy").setup({
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "solarized-osaka",
+      }
     },
     -- import any extras modules here
     -- disabling this extra because eslint-lsp uses way too much RAM
@@ -21,7 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,5 @@
 return {
-  { "folke/neodev.nvim", enabled = false },
   { "iamcco/mardown-preview.nvim", enabled = false },
   { "stevearc/dressing.nvim", enabled = false },
-  { "L3MON4D3/LuaSnip", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
+  {"folke/tokyonight.nvim", enabled = false},
 }
