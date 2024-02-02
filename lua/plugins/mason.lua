48a23c64cd628f62e1d2c38f4ff661813435caf1
BPagoaga <bernard.pagoaga@gmail.com>
2024-02-02 07:53:13 +0100

    add sonarlint-language-server
    but need more config to work ?

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,17 +2,16 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
-      -- "eslint-lsp",
+      "eslint-lsp",
       "html-lsp",
       "json-lsp",
       "marksman",
       "emmet-language-server",
       "prettier",
-      "eslint_d",
       "typescript-language-server",
       "yaml-language-server",
-      "stylua",
       "css-lsp",
+      "sonarlint-language-server",
     },
   },
 }
