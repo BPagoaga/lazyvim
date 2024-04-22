31a9c4822cbd6b84e5f23713c8aa491f5aa5df20
BPagoaga <bernard.pagoaga@gmail.com>
2024-04-22 23:06:18 +0200

    open definition in side split

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- change a keymap
+    keys[#keys + 1] = { "gd", ":vsplit | lua vim.lsp.buf.definition()<CR>" }
+  end,
   opts = {
     setup = {
       tsserver = function()
