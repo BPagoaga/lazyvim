a0854c17a0a12fb692cab4f29fa5603948fe4a86
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2024-05-30 14:27:23 +0200

    set search in the middle of the screen

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,6 +3,7 @@ return {
   opts = function(_, opts)
     opts.presets.lsp_doc_border = true
     opts.presets.command_palette = false
+    opts.presets.bottom_search = false
     -- opts.views = {
     --
     --   hover = {
