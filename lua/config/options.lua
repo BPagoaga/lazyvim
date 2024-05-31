29e5d56368dd1dbb6b15ccc4404566825e402c5b
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2024-05-31 21:32:02 +0200

    disable inlay hints

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,6 @@ vim.opt.scrolloff = 8 -- Lines of context
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- this is too buggy rn
+vim.lsp.inlay_hint.enable(false)
